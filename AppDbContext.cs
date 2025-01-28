dc3aa121994812701d907ca0e7f1c2d1af89a9f7
Édouard Renaud <edouard.renaud@polymtl.ca>
2025-01-28 15:53:20 -0500

    ajout de gitignore

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GestionEmployes.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace YourProjectName.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }  // Table "Employees"
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionEmployes.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }  // Clé primaire pour la base de donnée SQL SERVER
+        public string FirstName { get; set; } //VARCHAR
+        public string LastName { get; set; } //VARCHAR
+        public string Department { get; set; } //VARCHAR
+        public string Email { get; set; } //VARCHAR
+        public DateTime HireDate { get; set; } //DATE
+    }
+}
